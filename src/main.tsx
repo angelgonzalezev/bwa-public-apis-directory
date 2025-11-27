ea5bbf37825d844a5fcc50b3d7310062697f443b
Angel Gonzalez <aangel.gg@hotmail.com>
2025-11-27 10:02:28 +0100

    Add Clarity in head

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Clarity from "@microsoft/clarity";
 
 import App from "./App.tsx";
 import "./index.css";
-
-const projectId = process.env.CLARITY_PROJECT_ID ?? "";
-
-Clarity.init(projectId);
-
-Clarity.identify("custom-id", "custom-session-id", "custom-page-id", "friendly-name"); // only custom-id is required
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
