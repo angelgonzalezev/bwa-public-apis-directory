28a2ab31c2706b0199a7e8f1627097a1af5cfe31
Angel Gonzalez <aangel.gg@hotmail.com>
2025-11-27 09:58:23 +0100

    Install Clarity

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Clarity from "@microsoft/clarity";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+import App from "./App.tsx";
+import "./index.css";
+
+const projectId = process.env.CLARITY_PROJECT_ID ?? "";
+
+Clarity.init(projectId);
+
+Clarity.identify("custom-id", "custom-session-id", "custom-page-id", "friendly-name"); // only custom-id is required
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<App />
+	</StrictMode>
 );
